d67b3c607c5ed355d83a7e09db659c58fdc787ca
Kevin Mok <kmok@redhat.com>
2021-10-26 13:06:48 -0400

    Test query Django backend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const number = 0
+  const query = `query {
+    tokenByNumber(number: 0) {
+      number
+      imageUrl
+      traits {
+        traitType {
+          name
+        }
+        name
+        rarity
+      }
+      score
+    }
+  }`
+
+  fetch('http://127.0.0.1:8000/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables: { number },
+    })
+  })
+    .then(r => r.json())
+    .then(data => console.log('data returned:', data));
+
   return (
     <div className="App">
       <header className="App-header">
