00fbe71a46ea3a7d525f17aa0c3874bb136ebb32
Kevin Mok <kmok@redhat.com>
2021-10-28 14:53:27 -0400

    Token link watermark

--- src/components/Token.js
+++ src/components/Token.js
@@ -1,6 +1,10 @@
+import { useLocation, Link } from "react-router-dom";
+
 import Traits from './Traits'
 
 const Token = ({ token }) => {
+  const domainName = "raritycheck.art"
+
   return (
     <div className="container flex justify-items-start">
       <img className="object-contain w-64 m-5 rounded-lg float-left" src={token.imageUrl} alt="Token Image"/>
@@ -15,12 +19,17 @@ const Token = ({ token }) => {
         </li>
         <li>
           <p className="inline font-semibold">Score</p>
-          : {token.score}
+          <p className="inline">: {token.score}</p>
         </li>
         <li>Traits (in order of rarity)
           <ul className="list-disc pl-4">
             <Traits traits={token.traits}/>
           </ul> 
+        </li>
+        <li className="list-none pt-1 pl-16 text-sm text-blue-300 opacity-60 underline">
+          <Link to={useLocation().pathname}>
+            {`${domainName}${useLocation().pathname}`}
+          </Link>
         </li>
       </ul> 
     </div>
